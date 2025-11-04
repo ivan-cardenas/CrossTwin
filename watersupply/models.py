5d7cb8cfca653d719336d53feda64572310efdd5
Ivan Cardenas <i.l.cardenasleon@utwente.nl>
2025-11-04 17:20:30 +0100

    Add ConsumptionCapita model with fields for region, year, consumption per capita, and last updated timestamp

--- watersupply/models.py
+++ watersupply/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils import timezone
+
+
 
 # Create your models here.
+class ConsumptionCapita(models.Model):
+    region = models.CharField(max_length=100)
+    year = models.IntegerField()
+    consumption_per_capita = models.FloatField()
+    last_updated = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f"{self.region} - {self.year}: {self.consumption_per_capita} L/person/day"
